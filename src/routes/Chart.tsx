9302251a40c6df07c14c401e14ce8dece55ab10c
JSG <72681851+amkso@users.noreply.github.com>
2024-12-06 14:18:41 +0900

    Finished crypto
    - save before delete crypto project

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,87 @@
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import Apexchart from "react-apexcharts";
+import { useRecoilValue } from "recoil";
+import { isDarkAtom } from "../atoms";
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
+interface ChartProps {
+  coinId: string;
+}
+
+function Chart({ coinId }:ChartProps) {
+  const isDark = useRecoilValue(isDarkAtom);
+  const {isLoading, data} = useQuery<IHistorical[]>(
+    ["ohlcv", coinId], 
+    () => fetchCoinHistory(coinId),
+    {
+      refetchInterval: 10000,
+    }
+  );
+
+  return (
+    <div>
+      {isLoading ? 
+        "Loading chart..." 
+          : 
+        <Apexchart 
+          type="line" 
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => parseFloat(price.close)) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: isDark ? "dark" : "light",
+            },
+            chart: {
+              height:300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "trasparent",
+              zoom: {enabled: false,},
+            },
+            grid: { show:false },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: { show: false },
+            xaxis: { 
+              labels: {show: false},
+              axisTicks: {show: false},
+              axisBorder: {show: false},
+              categories: data?.map((price) => new Date(price.time_close * 1000).toUTCString()),
+            },
+            fill:{ 
+              type: "gradient", 
+              gradient: { gradientToColors: ["#0be881"], stops: [0,100],},
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$ ${value.toFixed(2)}`,
+              }
+            }
+          }} 
+        />
+      }
+    </div>
+  );
+  }
+  
+  export default Chart;
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,17 @@
 import { DefaultTheme } from "styled-components/dist/types";
 
-export const theme:DefaultTheme = {
-    bgColor:"white",
-    textColor:"black",
-    btnColor:"tomato",
+export const darkTheme: DefaultTheme = {
+    bgColor: "#2f3640",
+    textColor: "white",
+    accentColor: "#9c88ff",
+    cardBgColor: "transparent",
 };
+    
+export const lightTheme: DefaultTheme = {
+    bgColor: "whitesmoke",
+    textColor: "black",
+    accentColor: "#9c88ff",
+    cardBgColor: "white",
+};
+
+
